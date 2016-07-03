5daacc8fa1f49a05e73f15a5750c7aabdc6c011e
Nishant Rayan <nishantrayan@gmail.com>
2016-07-03 18:25:07 +0000

    Backout function

--- obstacle_avoidance.py
+++ obstacle_avoidance.py
@@ -9,24 +9,44 @@ MOTOR_MOVE_IN1 = 13
 MOTOR_MOVE_IN2 = 15
 MOTOR_MOVE_ENA1 = 7
 MOTOR_MOVE_ENA2 = 11
+MOTOR_DIR_ENA1 = 16
+MOTOR_DIR_ENA2 = 18
+MOTOR_DIR_IN1 = 22
+MOTOR_DIR_IN2 = 24
 ##setup inputs and outputs
 gpio.setup(IR_LEFT, gpio.IN)
-for out_pin in [MOTOR_MOVE_IN1, MOTOR_MOVE_IN2, MOTOR_MOVE_ENA1, MOTOR_MOVE_ENA2]:
+for out_pin in [MOTOR_MOVE_IN1, MOTOR_MOVE_IN2, MOTOR_MOVE_ENA1, MOTOR_MOVE_ENA2, MOTOR_DIR_IN1, MOTOR_DIR_IN2, MOTOR_DIR_ENA1, MOTOR_DIR_ENA2]:
     gpio.setup(out_pin, gpio.OUT)
-gpio.output(MOTOR_MOVE_ENA1, True)
-gpio.output(MOTOR_MOVE_ENA2, True)
-ena1 = gpio.PWM(MOTOR_MOVE_ENA1, 100)
-ena2 = gpio.PWM(MOTOR_MOVE_ENA2, 100)
-ena1.start(30)
-ena2.start(30)
+
+for ena_pin in [MOTOR_MOVE_ENA1, MOTOR_MOVE_ENA2]:
+    enas.append(gpio.PWM(ena_pin, 100))
+
+for ena_pin in enas:
+    ena_pin.start(30)
 
 
 def forward():
+    gpio.output(MOTOR_MOVE_ENA1, True)
+    gpio.output(MOTOR_MOVE_ENA2, True)
     gpio.output(MOTOR_MOVE_IN1, False)
     gpio.output(MOTOR_MOVE_IN2, True)    
 
 def stop():
+    gpio.output(MOTOR_MOVE_ENA1, False)
+    gpio.output(MOTOR_MOVE_ENA2, False)
+    gpio.output(MOTOR_DIR_ENA1, False)
+    gpio.output(MOTOR_DIR_ENA2, False)
     gpio.output(MOTOR_MOVE_IN1, False)
+    gpio.output(MOTOR_MOVE_IN2, False)
+
+def backout():
+    gpio.output(MOTOR_DIR_ENA1, True)
+    gpio.output(MOTOR_DIR_ENA2, True)
+    gpio.output(MOTOR_MOVE_ENA1, True)
+    gpio.output(MOTOR_MOVE_ENA2, True)
+    gpio.output(MOTOR_DIR_IN1, True)
+    gpio.output(MOTOR_DIR_IN2, False)
+    gpio.output(MOTOR_MOVE_IN1, True)
     gpio.output(MOTOR_MOVE_IN2, False)
     
 for x in range(0, 1):
@@ -34,5 +54,7 @@ for x in range(0, 1):
     time.sleep(2)
     stop()
     time.sleep(1)
-gpio.output(MOTOR_MOVE_ENA1, False)
-gpio.output(MOTOR_MOVE_ENA2, False)
+    backout()
+    time.sleep(2)
+    
+stop()
