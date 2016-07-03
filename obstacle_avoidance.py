dd25012da8bf7533b57ef4aea60ec67bb7b24cf2
Nishant Rayan <nishantrayan@gmail.com>
2016-07-03 18:55:13 +0000

    IR sense and backout

--- obstacle_avoidance.py
+++ obstacle_avoidance.py
@@ -17,17 +17,36 @@ MOTOR_DIR_IN2 = 24
 gpio.setup(IR_LEFT, gpio.IN)
 for out_pin in [MOTOR_MOVE_IN1, MOTOR_MOVE_IN2, MOTOR_MOVE_ENA1, MOTOR_MOVE_ENA2, MOTOR_DIR_IN1, MOTOR_DIR_IN2, MOTOR_DIR_ENA1, MOTOR_DIR_ENA2]:
     gpio.setup(out_pin, gpio.OUT)
-
+enas = []
 for ena_pin in [MOTOR_MOVE_ENA1, MOTOR_MOVE_ENA2]:
     enas.append(gpio.PWM(ena_pin, 100))
 
 for ena_pin in enas:
     ena_pin.start(30)
 
+def detect_obstacle():
+    max_confidence = 10
+    confidence = max_confidence
+    while True:
+        ir_left_in = gpio.input(IR_LEFT)
+        if ir_left_in == 0:
+            print "Detected"
+            print confidence
+            confidence = confidence - 1
+            if confidence == 0:
+              return True
+        else:
+            confidence = max_confidence
+        time.sleep(0.1)    
+            
 
 def forward():
+    gpio.output(MOTOR_DIR_ENA1, True)
+    gpio.output(MOTOR_DIR_ENA2, True)
     gpio.output(MOTOR_MOVE_ENA1, True)
     gpio.output(MOTOR_MOVE_ENA2, True)
+    gpio.output(MOTOR_DIR_IN1, False)
+    gpio.output(MOTOR_DIR_IN2, False)
     gpio.output(MOTOR_MOVE_IN1, False)
     gpio.output(MOTOR_MOVE_IN2, True)    
 
@@ -39,7 +58,7 @@ def stop():
     gpio.output(MOTOR_MOVE_IN1, False)
     gpio.output(MOTOR_MOVE_IN2, False)
 
-def backout():
+def backup():
     gpio.output(MOTOR_DIR_ENA1, True)
     gpio.output(MOTOR_DIR_ENA2, True)
     gpio.output(MOTOR_MOVE_ENA1, True)
@@ -49,12 +68,10 @@ def backout():
     gpio.output(MOTOR_MOVE_IN1, True)
     gpio.output(MOTOR_MOVE_IN2, False)
     
-for x in range(0, 1):
-    forward()
-    time.sleep(2)
+while True:
+    forward()    
+    detect_obstacle()
+    backup()
+    time.sleep(5)
     stop()
     time.sleep(1)
-    backout()
-    time.sleep(2)
-    
-stop()
