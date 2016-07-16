985c417f63da8d36ec0c9420435227bb260ec830
Nishant Rayan <nishantrayan@gmail.com>
2016-07-15 21:33:59 -0700

    Sample ultrasonic

--- sample/ultrasonic_sensor.py
+++ sample/ultrasonic_sensor.py
@@ -3,8 +3,8 @@ import time
 
 ## Ultra sonic sensor
 gpio.setmode(gpio.BOARD)
-TRIG = 12
-ECHO = 7
+TRIG = 31
+ECHO = 32
 
 gpio.setup(TRIG, gpio.OUT)
 gpio.output(TRIG, False)
@@ -13,14 +13,17 @@ gpio.setup(ECHO, gpio.IN)
 time.sleep(0.1)
 print "Starting measurement"
 
-gpio.output(TRIG, True)
-time.sleep(0.00001)
-gpio.output(TRIG, False)
-while gpio.input(ECHO) == 0:
+while True:
+  time.sleep(1)
+  gpio.output(TRIG, True)
+  time.sleep(0.00001)
+  gpio.output(TRIG, False)
+  while gpio.input(ECHO) == 0:
     pass
-start = time.time()
-while gpio.input(ECHO) == 1:
+  start = time.time()
+  while gpio.input(ECHO) == 1:
     pass
-stop = time.time()
-print (stop - start) * 170000
+  stop = time.time()
+  print (stop - start) * 170000
+
 gpio.cleanup()
