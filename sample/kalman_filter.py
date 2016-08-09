c0f7a931cb35d93887043fd5d27f78e34677d4aa
Nishant Rayan <nishantrayan@gmail.com>
2016-08-08 22:46:46 -0700

    kalman filter python code

---
+++ sample/kalman_filter.py
@@ -0,0 +1,26 @@
+class KalmanFilter(object):
+
+    def __init__(self, process_variance, estimated_measurement_variance):
+        self.process_variance = process_variance
+        self.estimated_measurement_variance = estimated_measurement_variance
+        self.posteri_estimate = 0.0
+        self.posteri_error_estimate = 1.0
+
+    def input_latest_noisy_measurement(self, measurement):
+        priori_estimate = self.posteri_estimate
+        priori_error_estimate = self.posteri_error_estimate + self.process_variance
+
+        blending_factor = priori_error_estimate / (priori_error_estimate + self.estimated_measurement_variance)
+        self.posteri_estimate = priori_estimate + blending_factor * (measurement - priori_estimate)
+        self.posteri_error_estimate = (1 - blending_factor) * priori_error_estimate
+
+    def get_latest_estimated_measurement(self):
+        return self.posteri_estimate
+
+
+filter = KalmanFilter(1, 3)
+with open("/home/pi/one_meter.txt") as f:
+  for line in f:
+    print line.strip()
+    filter.input_latest_noisy_measurement(int(line))
+    print "Estimate:", filter.get_latest_estimated_measurement()	
