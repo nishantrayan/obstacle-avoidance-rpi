73e38a7060884a8a5fb88388d98821a9d013f723
Nishant Rayan <nrayan@linkedin.com>
2016-08-19 12:13:14 -0700

    Moving parallel to obstacle (untested)

--- __init__.py
+++ __init__.py
@@ -5,4 +5,8 @@ print "Starting up car"
 if __name__ == '__init__':
     motor.move_forward(100)
     angle = sensor.sense_angles()
-    motor.move_backward()
+    if angle >= 90:
+        motor.turn_right(angle - 90)
+    else:
+        motor.turn_left(angle + 90)
+    motor.move_forward()
