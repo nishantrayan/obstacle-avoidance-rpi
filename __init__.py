9d293b80f3b7c5f50f1d6e16cc767ab4eb728c49
Nishant Rayan <nishantrayan@gmail.com>
2016-08-20 15:34:24 -0700

    Minor fixes to motor module

--- __init__.py
+++ __init__.py
@@ -1,12 +1,19 @@
 import motor
 import sensor
+import time
+import RPi.GPIO as gpio
 print "Starting up car"
 
-if __name__ == '__init__':
-    motor.move_forward(100)
-    angle = sensor.sense_angles()
-    if angle >= 90:
-        motor.turn_right(angle - 90)
-    else:
-        motor.turn_left(angle + 90)
-    motor.move_forward()
+motor.move_forward(100)
+time.sleep(2)
+motor.stop_left()
+motor.stop_right()
+motor.turn_right(10)
+time.sleep(2)
+gpio.cleanup()
+    #angle = sensor.sense_angles()
+    #if angle >= 90:
+    #    motor.turn_right(angle - 90)
+    #else:
+    #    motor.turn_left(angle + 90)
+    #motor.move_forward()
--- motor.py
+++ motor.py
@@ -45,6 +45,7 @@ def move_right_side(speed, dir='backward'):
 def move_forward(speed):
     stop_left()
     stop_right()
+    print "Moving forward"
     move_left_side(speed)
     move_right_side(speed)
 
@@ -80,6 +81,9 @@ def turn_right(angle):
 
 if __name__ != '__motor__':
     print "Initializing motor"
+    gpio.setwarnings(False)
+    gpio.setmode(gpio.BOARD)
+
     for out_pin in [L_IN1, L_IN2, L_ENA1, L_ENA2, R_IN1, R_IN2, R_ENA1, R_ENA2]:
         gpio.setup(out_pin, gpio.OUT)
 
